ebd1f8aec6a5d00389154a22451128efc76701f9
Joan Moncho <joanmonvi@gmail.com>
2022-12-02 19:04:05 +0100

    imagen EntidadRelacion

---
+++ src/main/java/com/joanmoncho/tiendavintage/TiendaVintageApplication.java
@@ -0,0 +1,13 @@
+package com.joanmoncho.tiendavintage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVintageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaVintageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joanmoncho/tiendavintage/TiendaVintageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joanmoncho.tiendavintage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVintageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
