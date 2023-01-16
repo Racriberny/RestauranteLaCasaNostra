1a6e650df950ea9e1e5520e5184f0ee552453fd1
Joan Moncho <joanmonvi@gmail.com>
2023-01-16 16:24:11 +0100

    actualizacion de algo

--- src/main/java/com/joanmoncho/tiendavintage/TiendaVintageApplication.java
+++ src/main/java/com/joanmoncho/tiendavintage/TiendaVintageApplication.java
@@ -1,12 +1,46 @@
 package com.joanmoncho.tiendavintage;
 
+import org.hibernate.Session;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.hibernate.SessionFactory;
+
+import javax.security.auth.login.Configuration;
 
 @SpringBootApplication
 public class TiendaVintageApplication {
     public static void main(String[] args) {
         SpringApplication.run(TiendaVintageApplication.class, args);
+
+        //StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
+
+        //SessionFactory sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+
+        //Session session = sf.openSession();
+
+        /*User user = new User();
+        user.setId(1);
+        user.setUserName("Pepe");
+        user.setUserMesagge("Hola mundo de Pepe");
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setUserName("Juan");
+        user2.setUserMesagge("Hola mundo de Juan");
+
+        session.getTransaction().begin();
+
+        session.save(user);
+        session.save(user2);
+
+        session.getTransaction().commit();
+
+        session.close();
+        sf.close();
+         */
     }
 
 }
---
+++ src/main/java/com/joanmoncho/tiendavintage/User.java
@@ -0,0 +1,44 @@
+package com.joanmoncho.tiendavintage;
+
+/*import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private int id;
+
+    private String userName;
+
+    private String userMesagge;
+
+    public User(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserMesagge() {
+        return userMesagge;
+    }
+
+    public void setUserMesagge(String userMesagge) {
+        this.userMesagge = userMesagge;
+    }
+
+}*/
